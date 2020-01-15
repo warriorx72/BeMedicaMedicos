46a136dcaf48f63c4b3e08a58aeffdb1180bced2
kamud <kamud@Pucci>
2020-01-15 16:15:51 -0600

    Cambios de funciones en JS, modificacion de radio button y reestructuracion de casa_habitacion

--- src/main/resources/static/js/deshabilitar.js
+++ src/main/resources/static/js/deshabilitar.js
@@ -1,4 +1,6 @@
-function deshabilitar(value, value2) {
+function deshabilitar(value, value2,value3) {
 					document.getElementById(value2).value = "";
 					document.getElementById(value2).disabled = true;
+					document.getElementById(value3).value = "";
+					document.getElementById(value3).disabled = true;
 				}
--- src/main/resources/static/js/habilitar.js
+++ src/main/resources/static/js/habilitar.js
@@ -1,4 +1,5 @@
-function habilitar(value, value2) {
+function habilitar(value, value2,value3) {
 					// habilitamos
 					document.getElementById(value2).disabled = false;
+					document.getElementById(value3).disabled = false;
 				}
